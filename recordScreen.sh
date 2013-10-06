4cde6a5607a30a59fee4a19eafc65b2144991475
will-v-king <iemailwill@gmail.com>
2013-10-06 15:18:29 +0800

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   recordScreen.sh
    	modified:   searchKeyWords
    
     Changes not staged for commit:
       (use "git add <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
       (commit or discard the untracked or modified content in submodules)
    
    	modified:   ssfp (modified content, untracked content)

--- recordScreen.sh
+++ recordScreen.sh
@@ -1,4 +1,7 @@
 #! /bin/bash -
 #  record screen within sound。
 # recordScreen.sh ${fileName}
-ffmpeg -f alsa -ac 2 -i default -f x11grab -s `xdpyinfo|grep 'dimensions:'|awk '{print $2}'` -r 20 -i :0.0 -sameq -strict experimental ${1}
+case "$2" in
+	"") ffmpeg -f alsa -ac 2 -i default -f x11grab -s `xdpyinfo|grep 'dimensions:'|awk '{print $2}'` -r 20 -i :0.0 -sameq -strict experimental ${1};;
+	*) ffmpeg -f alsa -ac 2 -i default -f x11grab -s "${2}" -r 20 -i :0.0"${3}" -sameq -strict experimental ${1};;
+esac
