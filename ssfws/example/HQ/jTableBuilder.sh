6f1e0c7b718d010e7855497a3cce84ebafd75825
will <will-v-king@hotmail.com>
2015-03-16 16:34:01 +0800

    support change date after minify. support build folder and src folder 	new file:   HQ/CMD 	new file:   HQ/CMDLST 	new file:   HQ/jTableBuilder.sh 	new file:   HQ/jTableCMD 	new file:   HQ/minCMD 	modified:   ../minifyJS.sh

--- ssfws/example/HQ/jTableBuilder.sh
+++ ssfws/example/HQ/jTableBuilder.sh
@@ -1,7 +1,23 @@
 #! /bin/bash -
-# minifyJS.sh
-# NOTICE the destination file must be end with min.js .;
-minifyJS(){
+# jTableBuilder.sh
+
+# Output usage information
+jTableBuilder(){
+	OutputUsage(){
+		echo "jTableBuilder works with UglifyJS2 .";
+		echo "Usage: `basename $0` [options...]";
+		echo "Options:";
+		echo -e " -p, --path Set the path to search files\n ";
+		echo -e " -l, --listFile (no argument follow) read source files direction from a list file.";
+		echo -e " -s, --sourceFile source file name; when -l is on, it mean the list file name. ";
+		echo -e " -d, --destinationFile the destination file name; when -d is on create option in the list file will be ignored.";
+		echo -e " -h, --help Display this help and exit";
+		echo "Example:";
+		echo "bash jTableBuilder.sh -l -p /workspace/jquery/collection/jtable/dev/ -s jquery.jtable.build.uglst -d jquery.jtable-min.js";
+		echo "now enjoy.";
+		echo "author: iemaiwill<at>gmail.com";
+		exit 1;
+	}
 	local path=".";
 	local listFile="";
 	local sourcePath="";
@@ -10,14 +26,13 @@ minifyJS(){
 	local destinationFile="";
 	local sourceFileDir="";
 	local destinationFileDir="";
-	local modifiedDate="200801310647";
 	local suffix="-debug.js";
 	## STARTRegion LongOption
 	# from /usr/share/doc/util-linux/examples/getopt-parse.bash
 	# Note that we use `"$@"' to let each command-line parameter expand to a 
 	# separate word. The quotes around `$@' are essential!
 	# We need TEMP as the `eval set --' would nuke the return value of getopt.
-	TEMP=`getopt -o p:le:s:n:d:x:m: --long path:,sourcePath:,sourceFile:,destinationPath:,destinationFile:,modifiedDate:,suffix: \
+	TEMP=`getopt -o p:le:s:n:d:x:h --long path:,sourcePath:,sourceFile:,destinationPath:,destinationFile:,suffix:,help \
 		 -n 'example.bash' -- "$@"`
 
 	if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
@@ -26,6 +41,7 @@ minifyJS(){
 
 	while true ; do
 		case "$1" in
+			-h|--help) OutputUsage ;;
 			-p|--path) path=$2 ; shift 2 ;;
 			-l|--listFile) listFile="yes" ; shift 1 ;;
 			-e|--sourcePath) sourcePath=$2 ; shift 2;;
@@ -39,7 +55,6 @@ minifyJS(){
 				#esac ;;
 			-n|--destinationPath) destinationPath=$2 ; shift 2 ;;
 			-d|--destinationFile) destinationFile=$2 ; shift 2 ;;
-			-m|--modifiedDate) modifiedDate=$2 ; shift 2 ;;
 			-x|--suffix) suffix=$2 ; shift 2 ;;
 			--) shift ; break ;;
 			*) echo "Internal error!" ; exit 1 ;;
@@ -91,23 +106,50 @@ minifyJS(){
 	destinationFileDir=${destinationPath}${destinationFile};
 	if [ -f ${sourceFileDir} ];
 	then
-		cd ${sourcePath};
+		#cd ${sourcePath};
 		# execute uglifyjs base on the list file.
 		if [ ${listFile} ];
 		then
-			sourceFileDir=$(cat ${sourceFileDir} | while read LINE; do echo -n $LINE" "; done;);
-			#uglifyjs -mt -ns -nc drop_debugger:false $(cat ${sourceFileDir} | while read LINE; do echo -n $LINE" "; done;) -o ${destinationFileDir} && uglifyjs -b beautify:false ${destinationFileDir} -o ${destinationPath}${destinationFile/%min.js/debug.js}
+			#sourceFileDir=$(cat ${sourceFileDir});
+			#echo "1""$sourceFileDir";
+			#echo $sourcePath;
+			local sourceFileList=${sourceFileDir};
+			mkdir -p `dirname ${destinationPath}`;
+			if [ ! -d ${destinationFileDir} ];
+			then
+				echo "" > ${destinationFileDir};
+			fi;
+			#sourceFileDir="";
+			#echo ${sourceFileList};
+			while read LINE ;
+			do
+				if [[ ("$LINE" == create\ *) && (${destinationFile} == "") ]];
+				then
+					LINE="${LINE/create\ /}";
+					echo ${destinationFileDir};
+					echo $LINE
+					destinationFileDir="${destinationFileDir}${LINE}";
+					destinationFileDir="$(readlink -f ${destinationPath}${LINE})";
+					echo ${destinationFileDir};
+					echo -n "" > ${destinationFileDir};
+				elif [[ $LINE == add\ * ]];
+				then
+					LINE=${LINE:4};
+					if [[ $LINE == *\.js ]];
+					then
+						uglifyjs $(readlink -f ${sourcePath}${LINE}) -m -c --comments '/^[\ \/]*\*\ \@/' -p 8 >> ${destinationFileDir};
+					else
+						uglifyjs $(readlink -f ${sourcePath}${LINE}) -m -c --comments '/.*/' -p 8 >> ${destinationFileDir};
+					fi;
+					echo "" >> ${destinationFileDir};
+				else
+					echo -e $LINE" is a surprise";
+				fi;
+			done < <(cat ${sourceFileList}) ;
+			echo ${destinationFileDir};
+		else
+				uglifyjs "${sourceFileDir}" -o ${destinationFileDir};
 		fi;
-		#uglifyjs -mt -ns -nc drop_debugger:false ${sourceFileDir} -o ${destinationFileDir} && uglifyjs -b beautify:false ${destinationFileDir} -o ${destinationPath}${destinationFile/%-min.js/${suffix}}
-		# for node-uglifyjs
-		# uglifyjs -ns -c --comments '^[\ \/]*\*\ \@' -b -o ${destinationPath}${destinationFile/%-min.js/-omg.js} ${sourceFileDir} && uglifyjs -mt -ns --source-map ${destinationPath}${destinationFile/%-min.js/-min.map} --source-map-url ${destinationPath}${destinationFile/%-min.js/-min.map} -c --comments '^[\ \/]*\*\ \@' -nc -o ${destinationFileDir} ${destinationPath}${destinationFile/%-min.js/-omg.js} && uglifyjs -mt -ns -c --comments '^[\ \/]*\*\ \@' -b -o ${destinationPath}${destinationFile/%-min.js/${suffix}} ${sourceFileDir}
-		# uglifyjs "${sourceFileDir}" -ns -c --comments '^[\ \/]*\*\ \@' -b -o '${destinationPath}${destinationFile/%-min.js/-omg.js}' && uglifyjs "${destinationPath}${destinationFile/%-min.js/-omg.js}" -mt -ns --source-map ${destinationPath}${destinationFile/%-min.js/-min.map} --source-map-url ${destinationPath}${destinationFile/%-min.js/-min.map} -c --comments '^[\ \/]*\*\ \@' -nc -o ${destinationFileDir} && uglifyjs "${sourceFileDir}" -mt -ns -c --comments '^[\ \/]*\*\ \@' -b -o ${destinationPath}${destinationFile/%-min.js/${suffix}}
-		uglifyjs ${sourceFileDir} -b -c --comments '/^[\ \/]*\*\ \@/' -p 8 > ${destinationPath}${destinationFile/%-min.js/-omg.js};
-		uglifyjs ${destinationPath}${destinationFile/%-min.js/-omg.js} -mt -ns --source-map ${destinationPath}${destinationFile/%-min.js/-min.map} --source-map-url ${destinationPath}${destinationFile/%-min.js/-min.map}  -c --comments '/^[\ \/]*\*\ \@/' -p 8 -nc > ${destinationFileDir};
-		uglifyjs ${sourceFileDir} -mt -ns -c --comments '/^[\ \/]*\*\ \@/' -p 8 -b > ${destinationPath}${destinationFile/%-min.js/${suffix}};
-		touch -t "${modifiedDate}" ${destinationFileDir}
-		touch -t "${modifiedDate}" ${destinationPath}${destinationFile/%-min.js/-omg.js}
-		touch -t "${modifiedDate}" ${destinationPath}${destinationFile/%-min.js/${suffix}}
 	fi;
 }
-minifyJS "$@";
+jTableBuilder "$@";
